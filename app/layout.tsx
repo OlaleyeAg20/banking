96c1fa219f325941101a3008a488ecda73c5993f
olaleyeag20 <olaleyeag.20@student.funaab.edu.ng>
2024-08-16 12:50:49 +0100

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import type { Metadata } from "next";
+import { Inter, IBM_Plex_Serif } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"], variable: '--font-inter' });
+const ibmPlexSerif = IBM_Plex_Serif({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-ibm-plex-serif'
+})
+
+export const metadata: Metadata = {
+  title: "Ayscript Bank",
+  description: "Ayscript is a modern banking platform for everyone",
+  icons: {
+    icon: '/icons/logo.svg'
+  }
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${ibmPlexSerif.variable} ${inter.variable}`}>{children}</body>
+    </html>
+  );
+}
